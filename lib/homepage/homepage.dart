d00a99ae7e721b5e9a79fd6e8dc71719d78ae553
yunusdemirli <yunus.demirli18@gmail.com>
2024-07-10 23:55:51 +0200

    bottomnavigationbar

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -8,10 +8,64 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+int Myindex = 1;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('data'),
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.blue,
+        title: const Center(child: Text("GYMBUDDY", style: TextStyle(fontWeight: FontWeight.bold),)),
+      ),
+      drawer: const Drawer(
+        child: Column(
+          children: [
+            Text('data1'),
+            Text('data2'),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            Myindex = index;
+          });
+        },
+        currentIndex: Myindex,
+        iconSize: 30,
+        selectedLabelStyle: const TextStyle(fontSize: 15),
+        unselectedLabelStyle: const TextStyle(fontSize: 15),
+        backgroundColor: Colors.blue,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.white,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.leaderboard),
+            label: "Stats",
+            backgroundColor: Colors.white,
+            ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fitness_center),
+            label: "Workout",
+            ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: "More",
+            ),
+        ]
+        ),
+
+      body: const Center(
+        child: Column(
+          children: [
+            Text('data')
+          ],
+        ),
+      ),
     );
   }
 }
