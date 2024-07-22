fa9f56fd03a0b04a3f049c910974662038bb5922
yunusdemirli <yunus.demirli18@gmail.com>
2024-07-22 23:20:55 +0200

    theme and snackbar

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -9,62 +9,72 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-int Myindex = 1;
+  int index = 1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //  --- APPBAR ---
       appBar: AppBar(
+        title: const Center(child: Text('GYMBUDDY', style: TextStyle(fontWeight: FontWeight.bold),)),
         foregroundColor: Colors.white,
-        backgroundColor: Colors.blue,
-        title: const Center(child: Text("GYMBUDDY", style: TextStyle(fontWeight: FontWeight.bold),)),
+        backgroundColor: Colors.indigo,
+        actions: [
+          IconButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text('data'),
+                  action: SnackBarAction(label: 'undo', onPressed:() {},),
+                  ),
+                );
+              },
+            icon: const Icon(Icons.info_outlined),
+            )
+        ],
       ),
+
+      //  --- DRAWER ---
       drawer: const Drawer(
         child: Column(
           children: [
-            Text('data1'),
-            Text('data2'),
+            Text('data'),
+            Text('data'),
           ],
         ),
       ),
+
+      //  --- BOTTOMNAVIGATIONBAR
       bottomNavigationBar: BottomNavigationBar(
-        onTap: (index) {
+        currentIndex: index,
+        onTap: (value) {
           setState(() {
-            Myindex = index;
+            index = value;
           });
         },
-        currentIndex: Myindex,
         iconSize: 30,
-        selectedLabelStyle: const TextStyle(fontSize: 15),
-        unselectedLabelStyle: const TextStyle(fontSize: 15),
-        backgroundColor: Colors.blue,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.white,
-        items: const <BottomNavigationBarItem>[
+        selectedItemColor: Colors.indigo,
+        unselectedItemColor: Colors.blue,
+        selectedLabelStyle: const TextStyle(fontSize: 18),
+        items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.leaderboard),
-            label: "Stats",
-            backgroundColor: Colors.white,
-            ),
-
+            icon: Icon(Icons.leaderboard), label: 'Stats'
+          ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.fitness_center),
-            label: "Workout",
-            ),
-
+            icon: Icon(Icons.fitness_center), label: 'Workout'
+          ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: "More",
-            ),
+            icon: Icon(Icons.add), label: 'More'
+          ),
         ]
-        ),
+      ),
 
-      body: const Center(
-        child: Column(
-          children: [
-            Text('data')
-          ],
-        ),
+      //  --- BODY ---
+      body: const Column(
+        children: [
+          Text('data'),
+          Text('data'),
+        ],
       ),
     );
   }
